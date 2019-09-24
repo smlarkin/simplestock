59c995f9a397bfaabd9b69109962dfee5bf92822
Shawn Larkin <shawnlarkin@Shawns-MacBook-Pro.local>
2019-09-24 17:54:56 -0400

    updated packages and deleted debug config

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Share, StyleSheet, View } from 'react-native'
-import { connect } from 'react-redux'
-import FooterIconButton from './FooterIconButton'
-import { updateDifferenceAndShopping, formatCategories } from '../util'
-import { setCategories, setShopping } from '../redux/actions'
+import React from 'react';
+import { Share, StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
+import FooterIconButton from './FooterIconButton';
+import { updateDifferenceAndShopping, formatCategories } from '../util';
+import { setCategories, setShopping } from '../redux/actions';
 
 const FooterViewsNav = ({
   categories,
@@ -13,13 +13,13 @@ const FooterViewsNav = ({
   shopping,
 }) => {
   function handleOnPressList() {
-    updateDifferenceAndShopping(categories, setCategories)
-    setShopping(false)
+    updateDifferenceAndShopping(categories, setCategories);
+    setShopping(false);
   }
 
   function handleOnPressCheckSquare() {
-    updateDifferenceAndShopping(categories, setCategories)
-    setShopping(true)
+    updateDifferenceAndShopping(categories, setCategories);
+    setShopping(true);
   }
 
   function share({ title, message }) {
@@ -31,21 +31,21 @@ const FooterViewsNav = ({
       // result.activityType === "com.apple.UIKit.activity.CopyToPasteboard"
       // DISMISSED
       // result.action === "dismissedAction"
-      .catch(error => console.error('error ', error))
+      .catch(error => console.error('error ', error));
   }
 
   function handleOnPressSend() {
     if (categoryIndex !== null && shopping) {
-      console.log('share category shopping list')
+      console.log('share category shopping list');
     } else if (categoryIndex !== null) {
-      console.log('share category inventory list')
+      console.log('share category inventory list');
     } else if (shopping) {
-      console.log('share entire shopping list')
+      console.log('share entire shopping list');
     } else {
-      console.log('share entire inventory list')
-      const title = 'All Inventory'
-      const message = formatCategories(categories)
-      share({ title, message })
+      console.log('share entire inventory list');
+      const title = 'All Inventory';
+      const message = formatCategories(categories);
+      share({ title, message });
     }
   }
   return (
@@ -72,8 +72,8 @@ const FooterViewsNav = ({
         visible={true}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -88,20 +88,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
-})
+});
 
 const mapStateToProps = state => ({
   categories: state.categories,
   categoryIndex: state.categoryIndex,
   shopping: state.shopping,
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setCategories: categories => dispatch(setCategories(categories)),
   setShopping: boolean => dispatch(setShopping(boolean)),
-})
+});
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(FooterViewsNav)
+  mapDispatchToProps,
+)(FooterViewsNav);
